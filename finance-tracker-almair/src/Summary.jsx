16aa0568a572df326a40734f2fd6be6b551b7bab
kramalmr <akram3107@student.abudzar.sch.id>
2025-05-14 12:04:22 +0700

    summary

--- finance-tracker-almair/src/Summary.jsx
+++ finance-tracker-almair/src/Summary.jsx
@@ -1,16 +1,24 @@
-export default function SummaryPage() {
-    localStorage.getItem('pemasukan') === null
-        ? localStorage.setItem('pemasukan', JSON.stringify([]))
-        : null;
+import { Link } from 'react-router-dom';
 
-    localStorage.getItem('pengeluaran') === null
-        ? localStorage.setItem('pengeluaran', JSON.stringify([]))
-        : null;
+export default function SummaryPage() {
+  const pemasukan = JSON.parse(localStorage.getItem('pemasukan')) || [];
+  const pengeluaran = JSON.parse(localStorage.getItem('pengeluaran')) || [];
+
+  const totalPemasukan = pemasukan
+  const totalPengeluaran = pengeluaran
 
   return (
     <div>
-      <h1>Summary</h1>
+      <Link to="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">
+        Back
+      </Link>
       
+      <h1>Summary</h1>
+      <p>Total Pemasukan: {totalPemasukan}</p>
+      <p>Total Pengeluaran: {totalPengeluaran}</p>
+      <p>Saldo: {totalPemasukan - totalPengeluaran}</p>
     </div>
   );
 }
+
+
